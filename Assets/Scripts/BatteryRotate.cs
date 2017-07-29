443603fe17460fa0ff076f20fe762401a3036d87
Hunter MacDonald <mactinite77@gmail.com>
2017-07-29 00:24:03 -0700

    add some of the models to the test scene, add rotation and bob script for battery and other collectibles

---
+++ Assets/Scripts/BatteryRotate.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryRotate : MonoBehaviour {
+
+    public float bobHeight = 0.25f;
+    public float bobFrequency = 0.5f;
+    public float rotSpeed;
+    private Vector3 offset;
+    private Vector3 originalPos;
+
+	// Use this for initialization
+	void Start () {
+        originalPos = transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        offset.y = bobHeight * Mathf.Cos(bobFrequency * Time.time);
+        transform.position = originalPos + offset;
+        transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
+
+	}
+}
