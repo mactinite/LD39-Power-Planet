ee94667baf38b99777264b1b466343d3cca47d0d
Hunter MacDonald <mactinite77@gmail.com>
2017-07-29 00:01:11 -0700

    Adding blaster and projectiles initial work

---
+++ Assets/Scripts/Blaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blaster : MonoBehaviour {
+
+    public Transform muzzlePosition;
+    public Transform projectilePrefab;
+    public float projectileSpeed;
+    public float projectileGravity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Spawn projectile and apply forces
+            Transform projectile = Instantiate(projectilePrefab, muzzlePosition.position, Quaternion.identity);
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 1000))
+            {
+                projectile.GetComponent<Rigidbody>().AddForce((hit.point - muzzlePosition.position).normalized * projectileSpeed);
+                Debug.Log("Hit");
+            }
+            else
+            {
+                projectile.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * projectileSpeed);
+                Debug.Log("No Hit");
+            }
+            
+        }
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public Transform hitFX;
+
+    private void Start()
+    {
+        Destroy(this.gameObject, 3.0f);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(hitFX, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
