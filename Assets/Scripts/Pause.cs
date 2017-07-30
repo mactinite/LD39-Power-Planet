94ab2e647cd9e99cec40ca0343c56936de141a62
Justin Ross <rossj190@gmail.com>
2017-07-30 09:17:30 -0700

    Added Pause menu Made minor changes to Blaster sound mechanics

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pause
+{
+
+    public class Pause : MonoBehaviour
+    {
+
+        [SerializeField] private GameObject pauseMenu;
+        bool paused = false;
+
+        void Start()
+        {
+            pauseMenu.SetActive(false);
+        }
+
+        void Update()
+        {
+            if (Input.GetButtonUp("Pause"))
+            {
+                if (!pauseMenu.activeInHierarchy)
+                {
+                    pauseGame();
+                }
+                else if (pauseMenu.activeInHierarchy)
+                {
+                    continueGame();
+                }
+            }
+        }
+        private void pauseGame()
+        {
+            Time.timeScale = 0;
+            paused = true;
+            pauseMenu.SetActive(true);
+        }
+        private void continueGame()
+        {
+            Time.timeScale = 1;
+            paused = false;
+            pauseMenu.SetActive(false);
+        }
+
+        public bool getPaused ()
+        {
+            return paused;
+        }
+    }
+}
