1696dc03936c1f795c0808c77ffd3f55d00c40f5
Hunter MacDonald <mactinite77@gmail.com>
2017-07-31 12:40:52 -0700

    moving platforms and hookshot slimes now conform to the laws of time, also they stop for a rest in between points. Travel is also now linear

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -9,11 +9,15 @@ public class MovingPlatform : MonoBehaviour {
     public bool active = false;
     public float moveTime = 10;
     private int currentTarget;
+    private int lastTarget;
     private bool backwards;
     private float timer = 0;
+    private float waitTimer;
+    public float waitTime = 1;
     // Use this for initialization
     void Start () {
-		
+        currentTarget = 0;
+        lastTarget = 0;
 	}
 	
 	// Update is called once per frame
@@ -22,33 +26,42 @@ public class MovingPlatform : MonoBehaviour {
         {
             if (Vector3.Distance(transform.position, path[currentTarget].position) > 0.25f)
             {
-                transform.localPosition =  Vector3.Lerp(transform.localPosition, path[currentTarget].localPosition , timer/moveTime );
-                timer += Time.deltaTime;
+                waitTimer = 0;
+                if (Time.timeScale != 0)
+                {
+                    transform.localPosition = Vector3.Lerp(path[lastTarget].localPosition, path[currentTarget].localPosition, timer / moveTime);
+                    timer += Time.deltaTime;
+                }
             }
             else
             {
                 timer = 0;
-                if (backwards)
+                waitTimer += Time.deltaTime;
+                if (backwards && waitTimer > waitTime)
                 {
                     if (currentTarget - 1 > 0)
                     {
+                        lastTarget = currentTarget;
                         currentTarget--;
                     }
                     else
                     {
                         backwards = false;
+                        lastTarget = currentTarget;
                         currentTarget++;
                     }
                 }
-                else
+                else if(!backwards && waitTimer > waitTime)
                 {
                     if (currentTarget + 1 < path.Length)
                     {
+                        lastTarget = currentTarget;
                         currentTarget++;
                     }
                     else
                     {
                         backwards = true;
+                        lastTarget = currentTarget;
                         currentTarget--;
                     }
                 }
