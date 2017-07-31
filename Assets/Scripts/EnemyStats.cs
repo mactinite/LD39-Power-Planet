3f615a6e4027aeb619c9c3f4fae9af644aea64e6
SleepyShaman <mshore@users.noreply.github.com>
2017-07-30 17:43:52 -0700

    Enemies can hurt the player through collision and Player can attack enemies with projectiles.

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour {
+
+	[Range(0,100)]
+	public float health = 30;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+	public bool ModifyHealth(float amount)
+	{
+		if(health + amount <= 100 && health + amount >= 0)
+		{
+			health += amount;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -9,11 +9,18 @@ public class MoveTo : MonoBehaviour {
 
 	public NavMeshAgent enemy;
 
+	public Rigidbody rb;
+
 	public Vector3 currentDestination;
+
+	public NavMeshPathStatus pathStatus;
 
 	// Use this for initialization
 	void Start () {
 		enemy = GetComponent<NavMeshAgent> ();
+		rb = GetComponent<Rigidbody> ();
+		rb.isKinematic = true;
+		rb.detectCollisions = false;
 		currentDestination = enemy.transform.position;
 		detectPlayer ();
 	}
@@ -28,13 +35,19 @@ public class MoveTo : MonoBehaviour {
 		NavMeshPath path = new NavMeshPath ();
 		Vector3 position = goal.position;
 		enemy.CalculatePath (position, path);
+		pathStatus = path.status;
 		if (path.status == NavMeshPathStatus.PathInvalid) {
 			goal = null;
 			if (enemy.transform.position.x == currentDestination.x && enemy.transform.position.z == currentDestination.z) {
+				rb.isKinematic = true;
+				rb.detectCollisions = false;
 				currentDestination = randomNavSphere (enemy.transform.position, 5.0f, -1);
 				enemy.destination = currentDestination;
 				return;
 			}
+		} else {
+			rb.isKinematic = false;
+			rb.detectCollisions = true;
 		}
 		currentDestination = enemy.transform.position;
 		enemy.destination = position;
@@ -47,4 +60,20 @@ public class MoveTo : MonoBehaviour {
 		NavMesh.SamplePosition (randomDirection, out navHit, distance, layermask);
 		return navHit.position;
 	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Player") {
+			GameObject player = col.gameObject;
+			player.GetComponent<PlayerStats> ().ModifyHealth (-10.0f);
+		}
+
+		if (col.gameObject.tag == "Player Projectile") {
+			this.GetComponent<EnemyStats> ().ModifyHealth (-10.0f);
+			if (this.GetComponent<EnemyStats> ().health == 0) {
+				Destroy (this.gameObject);
+			}
+
+		}
+	}
+		
 }
